2369c891fbb3e0e579c6941f836c341bceddd243
KrayMakso68 <makson6803@gmail.com>
2024-09-30 21:45:07 +0300

    minor commit

--- tgbot_server/app/repository/base_repository.py
+++ tgbot_server/app/repository/base_repository.py
@@ -1,15 +1,28 @@
-from typing import TypeVar, AsyncGenerator, Type
+from typing import TypeVar, AsyncGenerator, Type, Callable
 
+from sqlalchemy.exc import IntegrityError
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from app.core.exceptions import DuplicatedError
 from app.model.base_model import BaseModel
 
 T = TypeVar("T", bound=BaseModel)
 
 
 class BaseRepository:
-    def __init__(self, async_session: AsyncGenerator[AsyncSession, None], model: Type[T]) -> None:
+    def __init__(self, async_session: Callable[..., AsyncGenerator[AsyncSession, None]], model: Type[T]) -> None:
         self.async_session = async_session
         self.model = model
 
-    async def create(self, schema: T):
+    async def create(self, schema: T) -> int:
+        async with self.async_session() as session:
+            query = self.model(**schema.dict())
+            try:
+                session.add(query)
+                session.commit()
+                session.refresh(query)
+            except IntegrityError as e:
+                raise DuplicatedError(detail=str(e.orig))
+            return query.id
+
+    # async def read_by_id(self, id: int) -> T:
--- tgbot_server/app/repository/user_repository.py
+++ tgbot_server/app/repository/user_repository.py
@@ -1,4 +1,4 @@
-from typing import AsyncGenerator
+from typing import AsyncGenerator, Callable
 
 from sqlalchemy.ext.asyncio import AsyncSession
 
@@ -8,6 +8,6 @@ from app.model.user import User
 
 
 class UserRepository(BaseRepository):
-    def __init__(self, async_session: AsyncGenerator[AsyncSession, None] = get_async_session):
+    def __init__(self, async_session: Callable[..., AsyncGenerator[AsyncSession, None]] = get_async_session):
         self.async_session = async_session
         super().__init__(async_session, User)
