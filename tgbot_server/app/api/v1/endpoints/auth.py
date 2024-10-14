2edb8a5df180367c7d95a9a31d457eb484c12d63
KrayMakso68 <makson6803@gmail.com>
2024-10-14 21:05:01 +0300

    code refactoring

--- tgbot_server/app/api/v1/endpoints/auth.py
+++ tgbot_server/app/api/v1/endpoints/auth.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Depends
 
-from app.schema.auth_schema import WebAppInitData, TokenInfo, InitAuthData
+from app.schema.auth_schema import TokenInfo, InitAuthData
 from app.schema.user_schema import UserSchema
 from app.core.dependencies import get_auth_service, get_current_active_user
 from app.services.auth_service import AuthService
@@ -12,10 +12,15 @@ router = APIRouter(
 
 
 @router.post("/login")
-async def login_for_access_token(init_data: InitAuthData = Depends(), service: AuthService = Depends(get_auth_service)) -> TokenInfo:
+async def login_for_access_token(
+        init_data: InitAuthData = Depends(),
+        service: AuthService = Depends(get_auth_service)
+) -> TokenInfo:
     return await service.login(init_data)
 
 
-@router.get("/testsecure")
-async def testsecure(current_user: UserSchema = Depends(get_current_active_user)):
+@router.get("/user")
+async def user(
+        current_user: UserSchema = Depends(get_current_active_user)
+) -> UserSchema:
     return current_user
