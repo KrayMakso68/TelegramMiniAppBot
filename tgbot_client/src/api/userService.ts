7578b20cc655a8475af226dba70b5a7606ce6213
KrayMakso68 <makson6803@gmail.com>
2024-10-28 20:52:14 +0300

    comments removed, error handler added

--- tgbot_client/src/api/userService.ts
+++ tgbot_client/src/api/userService.ts
@@ -11,32 +11,15 @@ export const getUserAvatarUrl = async (): Promise<string | null> => {
   const cachedAvatarBase64: string | null = LocalStorage.getItem<string>('userAvatar');
 
   if (!cachedAvatarBase64) {
-    const response = await api.get(`/user/avatar`);
-    const avatarBase64: string = response.data;
-    LocalStorage.set('userAvatar', avatarBase64);
-    return avatarBase64;
+    try {
+      const response = await api.get(`/user/avatar`);
+      const avatarBase64: string = response.data;
+      LocalStorage.set('userAvatar', avatarBase64);
+      return avatarBase64;
+    } catch (error) {
+      console.error("Error getting avatar from server:", error);
+      return null;
+    }
   }
-
   return cachedAvatarBase64;
-
-
-  // if (cachedAvatarBase64) {
-  //   await fetch(cachedAvatarUrl)
-  //     .then(() => cachedAvatarUrl)
-  //     .catch((error) => {
-  //       console.error("Ошибка проверки аватара:", error);
-  //       URL.revokeObjectURL(cachedAvatarUrl);
-  //       LocalStorage.remove('userAvatar');
-  //     })
-  // }
-  // try {
-  //   const response = await api.get(`/user/avatar`, {responseType: 'blob'});
-  //   const avatarUrl: string = URL.createObjectURL(response.data);
-  //   LocalStorage.set('userAvatar', avatarUrl);
-  //   return avatarUrl;
-  // } catch (error) {
-  //   console.error("Ошибка при получении аватара с сервера:", error);
-  //   return null;
-  // }
-
 };
