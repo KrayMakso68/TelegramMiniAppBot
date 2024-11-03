d3bf177bfae930d051953e8157b5215610164f67
KrayMakso68 <makson6803@gmail.com>
2024-11-04 00:47:17 +0300

    added sub_uuid column in users table

--- tgbot_server/app/model/user_model.py
+++ tgbot_server/app/model/user_model.py
@@ -1,4 +1,6 @@
-from sqlalchemy import Integer, Boolean
+import uuid
+
+from sqlalchemy import Integer, Boolean, String
 from sqlalchemy.orm import Mapped, mapped_column
 
 from app.model.base_model import BaseModel
@@ -8,5 +10,6 @@ class User(BaseModel):
     __tablename__ = 'users'
 
     tg_id: Mapped[int] = mapped_column(Integer, nullable=False, unique=True)
+    sub_uuid: Mapped[str] = mapped_column(String, nullable=False, unique=True, default=lambda: str(uuid.uuid4()))
     is_active: Mapped[bool] = mapped_column(Boolean, default=True)
     is_superuser: Mapped[bool] = mapped_column(Boolean, default=False)
--- tgbot_server/app/schema/user_schema.py
+++ tgbot_server/app/schema/user_schema.py
@@ -3,9 +3,9 @@ from app.schema.base_schema import ModelBaseInfo, BaseSchema
 
 class UserCreate(BaseSchema):
     tg_id: int
-    is_active: bool = True
-    is_superuser: bool = False
 
 
 class UserSchema(ModelBaseInfo, UserCreate):
-    ...
+    sub_uuid: str
+    is_active: bool
+    is_superuser: bool
