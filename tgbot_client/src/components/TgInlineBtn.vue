67c39f95053d5cf918fe3143e8ab0ce1bb9ec21a
KrayMakso68 <makson6803@gmail.com>
2024-11-10 21:05:41 +0300

    add buttons in balance section

---
+++ tgbot_client/src/components/TgInlineBtn.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+
+interface Props {
+  icon?: string;
+  to?: string;
+  label: string;
+}
+
+defineProps<Props>();
+
+const emit = defineEmits<{
+  (e: 'click'): void;
+}>();
+
+const handleClick = () => {
+  emit('click');
+};
+</script>
+
+<template>
+  <q-btn :to="to" rounded stack flat unelevated no-caps draggable="false">
+    <div class="flex flex-center column tg-inline-btn">
+      <q-icon size="md" :name=icon />
+      <span>{{ label }}</span>
+    </div>
+  </q-btn>
+</template>
+
+<style scoped>
+.tg-inline-btn {
+  font-weight: 500;
+  color: var(--tg-accent-text-color);
+}
+</style>
--- tgbot_client/src/components/WalletBalance.vue
+++ tgbot_client/src/components/WalletBalance.vue
@@ -1,13 +1,14 @@
 <script setup lang="ts">
 import {ref} from "vue";
+import TgInlineBtn from "components/TgInlineBtn.vue";
 
-const amount = ref<number | null>(null)
+const amount = ref<number | null>(500)
 </script>
 
 <template>
-  <div class="row justify-start items-center q-px-lg q-gutter-sm">
+  <div class="row justify-start items-center q-px-lg q-pt-sm q-gutter-sm">
     <div class="col-auto tg-subtitle-text">
-      <div class="text-h4 text-weight-regular">₽</div>
+      <div class="text-h4 text-weight-medium">₽</div>
     </div>
     <div v-if="amount" class="col-auto tg-text">
       <div class="text-h4 text-weight-regular">{{ amount }}</div>
@@ -16,10 +17,9 @@ const amount = ref<number | null>(null)
       <q-skeleton type="rect"/>
     </div>
   </div>
-  <div class="row justify-center">
-    <div class="col col-auto">
-      <q-btn class="tg-button">Пополнить</q-btn>
-    </div>
+  <div class="flex row justify-evenly q-pa-sm q-gutter-sm">
+    <tg-inline-btn to="test-colors" class="col" label="Пополнить" icon="add_circle"/>
+    <tg-inline-btn to="" class="col" label="История" icon="history"/>
   </div>
 
 </template>
