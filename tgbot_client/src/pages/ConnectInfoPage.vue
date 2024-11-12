31e177009ed197408c71a363588ede3aea234ec6
KrayMakso68 <makson6803@gmail.com>
2024-11-12 21:48:00 +0300

    add connect info route

---
+++ tgbot_client/src/pages/ConnectInfoPage.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import {BackButton} from "vue-tg";
+import { useRouter} from "vue-router";
+import {Connect} from "src/api/types/subscribeTypes";
+
+const router = useRouter();
+
+defineProps<Connect>()
+</script>
+
+<template>
+  <BackButton @click="() => router.back()"></BackButton>
+  <q-page padding class="text-green-6 ">
+    <div class="column">
+      <q-card class="col row">
+        <q-card-section>
+          {{email}}
+        </q-card-section>
+      </q-card>
+    </div>
+    <div class="column">
+      <q-card class="col row">
+        <q-card-section>
+          {{inboundName}}
+        </q-card-section>
+      </q-card>
+    </div>
+  </q-page>
+</template>
+
+<style scoped>
+
+</style>
--- tgbot_client/src/router/routes.ts
+++ tgbot_client/src/router/routes.ts
@@ -5,9 +5,29 @@ const routes: RouteRecordRaw[] = [
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
     children: [
-      { path: '', component: () => import('pages/IndexPage.vue') },
-      { path: 'not-from-telegram', component: () => import('pages/errors/NotFromTelegram.vue') },
-      { path: 'test-colors', component: () => import('pages/TelegramColors.vue') },
+      {
+        path: '',
+        component: () => import('pages/IndexPage.vue')
+      },
+      {
+        path: 'not-from-telegram',
+        component: () => import('pages/errors/NotFromTelegram.vue')
+      },
+      {
+        path: 'test-colors',
+        component: () => import('pages/TelegramColors.vue')
+      },
+      {
+        path: 'connect-info',
+        component: () => import('pages/ConnectInfoPage.vue'),
+        props: (route) => ({
+          connectUrl: route.query.connectUrl as string,
+          uuid: route.query.uuid as string,
+          email: route.query.email as string,
+          inboundName: route.query.inboundName as string
+        }),
+        meta: { transition: 'slide-right' },
+      },
     ],
   },
   {
