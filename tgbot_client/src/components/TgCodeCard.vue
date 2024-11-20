48a8b9dd7cbf1ffc4ca75f32ba69892cbc891b0d
KrayMakso68 <makson6803@gmail.com>
2024-11-20 17:26:17 +0300

    create code view telegram element

---
+++ tgbot_client/src/components/TgCodeCard.vue
@@ -0,0 +1,101 @@
+<script setup lang="ts">
+import {copyToClipboard, useQuasar} from "quasar";
+
+interface Props {
+  title?: string | undefined,
+  text: string
+}
+
+const props = defineProps<Props>()
+const $q = useQuasar();
+
+const onCopy = () => {
+  copyToClipboard(props.text)
+    .then(() => {
+      $q.notify({
+        type: "positive",
+        message: "Текст скопирован!",
+        timeout: 1000,
+      });
+    })
+    .catch(() => {
+      $q.notify({
+        type: "negative",
+        message: "Ошибка при копировании текста.",
+        timeout: 1000,
+      });
+    });
+};
+</script>
+
+<template>
+  <div class="copy-container">
+    <q-card flat class="copy-card">
+
+      <q-card-section class="q-pa-none q-pl-md transparent-background">
+        <div class="text-caption text-no-wrap">
+          {{ title }}
+        </div>
+      </q-card-section>
+
+      <q-card-section class="q-py-none">
+        <div class="text-content">
+          <code>
+            <pre>{{ text }}</pre>
+          </code>
+        </div>
+      </q-card-section>
+
+      <q-card-actions vertical class="q-pa-none">
+        <q-btn style="color: var(--tg-button-color);"
+               flat
+               size="sm"
+               icon="content_copy"
+               @click="onCopy"
+               label="Copy"
+        />
+      </q-card-actions>
+
+    </q-card>
+  </div>
+</template>
+
+<style scoped>
+.copy-container {
+
+}
+
+.copy-card {
+  width: 100%;
+  max-width: 600px;
+  background-color: var(--tg-section-bg-color);
+  border-radius: 4px;
+  border-left: 3px solid var(--tg-accent-text-color);
+}
+
+pre {
+  color: var(--tg-link-color);
+  font-size: 12px;
+  white-space: pre-wrap;
+  word-wrap: break-word;
+}
+
+.transparent-background {
+  position: relative;
+  color: var(--tg-accent-text-color);
+  z-index: 0;
+}
+
+.transparent-background::before {
+  content: '';
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: var(--tg-accent-text-color);
+  border-top-right-radius: 4px;
+  opacity: 0.2;
+  z-index: -1;
+}
+</style>
--- tgbot_client/src/pages/ConnectInfoPage.vue
+++ tgbot_client/src/pages/ConnectInfoPage.vue
@@ -2,97 +2,33 @@
 import {BackButton} from "vue-tg";
 import {useRouter} from "vue-router";
 import {Connect} from "src/api/types/subscribeTypes";
-import {copyToClipboard, useQuasar} from "quasar";
 import TgSection from "components/TgSection.vue";
+import TgCodeCard from "components/TgCodeCard.vue";
 
-
-const $q = useQuasar();
 const router = useRouter();
-
-const props = defineProps<Connect>()
-
-
-const onCopy = () => {
-  copyToClipboard(props.connectUrl)
-    .then(() => {
-      $q.notify({
-        type: "positive",
-        message: "Текст скопирован!",
-        timeout: 1000,
-      });
-    })
-    .catch(() => {
-      $q.notify({
-        type: "negative",
-        message: "Ошибка при копировании текста.",
-        timeout: 1000,
-      });
-    });
-};
+defineProps<Connect>()
 </script>
 
 <template>
   <BackButton @click="() => router.back()"></BackButton>
-  <q-page padding>
+  <q-page class="tg-text overflow-auto">
+    <div class="q-gutter-y-md">
+      <tg-section>
+        <div class="text-h5 text-center">
+          {{email}}
+        </div>
+      </tg-section>
 
-      <div class="copy-container">
-        <q-card flat class="copy-card">
+      <tg-code-card title="Connection url" :text=connectUrl class=""/>
 
-          <q-card-section
-            class="q-pa-none q-pl-md"
-            style="background-color: var(--tg-button-color); border-top-left-radius: 0px; color: var(--tg-accent-text-color);"
-          >
-            <div class="text-caption text-no-wrap">
-              Connection url
-            </div>
-          </q-card-section>
+      <tg-section label="Расход трафика">
+        dsfsd
+      </tg-section>
 
-          <q-card-section class="q-py-none">
-            <div class="text-content">
-              <code>
-                <pre style="color: var(--tg-link-color);">{{ connectUrl }}</pre>
-              </code>
-            </div>
-          </q-card-section>
-
-          <q-card-actions vertical class="q-pa-none">
-            <q-btn style="color: var(--tg-button-color);"
-                   flat
-                   size="sm"
-                   icon="content_copy"
-                   @click="onCopy"
-                   label="Copy"
-            />
-          </q-card-actions>
-
-        </q-card>
-      </div>
-    <tg-section label="test" class="q-mt-lg">
-      <div>
-        fdfd
-      </div>
-    </tg-section>
+    </div>
   </q-page>
 </template>
 
 <style scoped>
-.copy-container {
-  display: flex;
-  justify-content: center;
-}
 
-.copy-card {
-  width: 100%;
-  max-width: 600px;
-  background-color: var(--tg-section-bg-color);
-  border-radius: 4px;
-  border-left: 3px solid var(--tg-accent-text-color);
-}
-
-.text-content pre {
-  font-size: 12px;
-  color: #333;
-  white-space: pre-wrap;
-  word-wrap: break-word;
-}
 </style>
