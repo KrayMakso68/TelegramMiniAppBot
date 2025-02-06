043c592ce12fa1545cd926b72668f56ce7c8cfa3
KrayMakso68 <makson6803@gmail.com>
2025-02-06 17:36:51 +0300

    added new server model

---
+++ tgbot_server/app/model/server_model.py
@@ -0,0 +1,24 @@
+from sqlalchemy import Integer, Float, String, Boolean, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from app.core.database import Base
+from app.model.base_model import BaseModel
+
+
+class Country(Base):
+    __tablename__ = 'countries'
+
+    code: Mapped[str] = mapped_column(String(2), primary_key=True)
+    name: Mapped[str] = mapped_column(String, nullable=False)
+
+    servers = relationship("Server", back_populates="country_rel")
+
+class Server(BaseModel):
+    __tablename__ = 'servers'
+
+    name: Mapped[str] = mapped_column(String, nullable=False)
+    ip_address: Mapped[str] = mapped_column(String, nullable=False)
+    country: Mapped[str] = mapped_column(String(2), ForeignKey("countries.code"), nullable=False)
+    is_active: Mapped[bool] = mapped_column(Boolean, nullable=False)
+
+    country_rel = relationship("Country", back_populates="servers")
